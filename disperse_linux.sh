b633c81ec567fe4e87732d46d561a3a9d8786b3f
Warwick Stone <uozu.aho@gmail.com>
2021-07-10 16:12:44 +1000

    reorg for linux

--- disperse_linux.sh
+++ disperse_linux.sh
@@ -3,8 +3,9 @@
 
 read -p "Change your name and email in .gitconfig.linux, then press any key"
 
-cp .bashrc.linux ~/.bashrc
+cp .bashrc.woz ~
 cp .bash.aliases ~
+cp .bash.aliases.xub ~
 cp .gitconfig.linux ~/.gitconfig
 cp vscode/vscode-keybindings.json ~/.config/Code/User/keybindings.json
 cp vscode/vscode-settings.json ~/.config/Code/User/settings.json
--- suck_linux.sh
+++ suck_linux.sh
@@ -2,8 +2,9 @@
 # Suck all config from the filesystem to this project.
 # Commit any working changes first!
 
-cp ~/.bashrc .bashrc.linux
-cp ~/.bash.aliases .bash.aliases
+cp ~/.bashrc.woz .
+cp ~/.bash.aliases .
+cp ~/.bash.aliases.xub .
 cp ~/.gitconfig .gitconfig.linux
 cp ~/.config/Code/User/keybindings.json vscode/vscode-keybindings.json
 cp ~/.config/Code/User/settings.json vscode/vscode-settings.json
