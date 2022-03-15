ba6a50efc9de49d3a1d0ff4c2187475b6fb4d013
Warwick Stone <Warwick.Stone@myob.com>
2022-03-15 20:53:32 +1100

    add separate aliases, suck, disperse

--- disperse_mac.sh
+++ disperse_mac.sh
@@ -1,3 +1,18 @@
 #!/bin/zsh
-# create an 'admin' command that can be run from spotlight
-echo code /Users/Warwick.Stone/Library/CloudStorage/OneDrive-MYOB/mega_links/notes > ~/admin.command
+function create_admin_cmd {
+  echo code /Users/Warwick.Stone/Library/CloudStorage/OneDrive-MYOB/mega_links/notes > ~/admin.command
+}
+
+function install_oh_my_zsh {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  rm ~/.zshrc
+}
+
+function copy_my_dotfiles {
+  cp .zshrc.mac ~/.zshrc
+  cp .woz.aliases.mac ~/.woz.aliases
+}
+
+create_admin_cmd
+install_oh_my_zsh
+copy_my_dotfiles
