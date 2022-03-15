b0d1c17a59a391975eec03a205b7e666e1c4a9fa
Warwick Stone <Warwick.Stone@myob.com>
2022-03-15 21:11:23 +1100

    more mac aliases

--- disperse_mac.sh
+++ disperse_mac.sh
@@ -1,16 +1,19 @@
 #!/bin/zsh
 function create_admin_cmd {
+  # you may need to run this once from finder for it to appear in spotlight
   echo code /Users/Warwick.Stone/Library/CloudStorage/OneDrive-MYOB/mega_links/notes > ~/admin.command
 }
 
 function install_oh_my_zsh {
+  # note: this overwrites ~/.zshrc
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  rm ~/.zshrc
 }
 
 function copy_my_dotfiles {
   cp .zshrc.mac ~/.zshrc
-  cp .woz.aliases.mac ~/.woz.aliases
+  cp .bash.aliases ~
+  cp .woz.aliases.mac ~
+  cp .woz.aliases.mac.myob ~
 }
 
 create_admin_cmd
