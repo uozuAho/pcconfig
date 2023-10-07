5e82cbacc4e0472352eb2ab48830691b8b3f5575
Warwick Stone <warwick_stone@fastmail.com>
2023-10-07 14:51:26 +1100

    disperse linux music

--- disperse_linux.sh
+++ disperse_linux.sh
@@ -7,5 +7,6 @@ cp .bashrc.woz ~
 cp .bash.aliases ~
 cp .bash.aliases.xub ~
 cp .gitconfig ~
+cp .music_youtubes ~
 cp vscode/vscode-keybindings.json ~/.config/Code/User/keybindings.json
 cp vscode/vscode-settings.json ~/.config/Code/User/settings.json
