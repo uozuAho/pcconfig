38bfe923617d7dbb208fab34d06ee54c49f95a32
Warwick Stone <warwick_stone@fastmail.com>
2023-10-07 12:18:54 +1100

    music

--- suck_windows.sh
+++ suck_windows.sh
@@ -6,5 +6,6 @@ cp ~/.bashrc .bashrc.woz
 cp ~/.bash.aliases .
 cp ~/.bash.aliases.windows .
 cp ~/.gitconfig .
+cp ~/.music_youtubes .
 cp ~/"AppData/Roaming/Code/User/keybindings.json" "vscode/vscode-keybindings.json"
 cp ~/"AppData/Roaming/Code/User/settings.json" "vscode/vscode-settings.json"
