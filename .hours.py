50aa4049435e554fe71d5f3c71bfd4cce739f3b3
Warwick Stone <warwick.stone@domain.com.au>
2024-02-06 17:15:24 +1100

    hours: add total

--- .hours.py
+++ .hours.py
@@ -14,6 +14,7 @@ import re
 rg = re.compile(r'\b(\d{3,4})\s*-\s*(\d{3,4})\b')
 
 def parse_time(time_str) -> int:
+    """ Returns time in minutes from midnight """
     if len(time_str) == 3:
         time_str = '0' + time_str
     hours = int(time_str[:2])
@@ -21,11 +22,14 @@ def parse_time(time_str) -> int:
     return hours * 60 + minutes
 
 
-def duration(start, end) -> int:
+def duration(start, end) -> float:
+    """ Returns time in hours """
     if end < start:
         end += 12 * 60
     return (end - start) / 60
 
+
+total = 0
 for line in sys.stdin:
     line = line.rstrip()
     matches = rg.findall(line)
@@ -36,4 +40,6 @@ for line in sys.stdin:
             start = parse_time(match[0])
             end = parse_time(match[1])
             hours = duration(start, end)
+            total += hours
             print(f'{line} : {hours:.1f}')
+print(f'{total:.1f}')
